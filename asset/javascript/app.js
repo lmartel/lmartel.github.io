cb86f1753bafb34552eaffa5c791888cb22dceed
Leo <github.com/lmartel>
2013-06-23 05:33:37 -0700

    resume and projects sections finished

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -1,6 +1,36 @@
 $(document).ready(function(){
   highlightTab();
+  $('.bxslider').bxSlider({
+    preloadImages: 'all', // different sizes
+    adaptiveHeight: true,
+    slideWidth: 800,
+    speed: 500,
+    onSlideNext: function(){
+      slideWithWrap(true);
+    },
+    onSlidePrev: function(){
+      slideWithWrap(false);
+    },
+  });
 });
+
+function slideWithWrap(forward){
+  $(".project-description.selected").fadeOut(150, function(){
+    $(this).removeClass("selected");
+
+    var newDesc;
+    if(forward){
+      newDesc = $(this).next(".project-description");
+      if(newDesc.size() === 0) newDesc = $(this).parent().find(".project-description").first();
+    } else {
+      newDesc = $(this).prev(".project-description");
+      if(newDesc.size() === 0) newDesc = $(this).parent().find(".project-description").last();
+    }
+    newDesc.fadeIn(350, function(){
+      $(this).addClass("selected");
+    })
+  });
+};
 
 function highlightTab(){
   var special = {'' : 'home'};
